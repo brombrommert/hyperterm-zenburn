ae99688c37572f5ca8d924f7f655933594544714
Rommert Zijlstra <rommert@thinkbright.nl>
2016-07-18 20:14:08 +0200

    update bright black color

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const foregroundColor = '#DCDCCC'
 const cursorColor = '#657B83'
 
 const colors = [
-  '#020202',
-  '#BC4C4C',
-  '#6C9C6C',
-  '#D8D856',
-  '#4B4BBD',
-  '#BD6CA4',
-  '#4BB7BD',
-  '#BFBFBF',
-  '#414141',
-  '#DC6C6C',
-  '#8CBC8C',
-  '#E6E600',
-  '#6B6BDB',
-  '#BE89AE',
-  '#91D8DB',
-  '#E6E6E6'
+  '#020202', // Black
+  '#BC4C4C', // Red
+  '#6C9C6C', // Green
+  '#D8D856', // Yellow
+  '#4B4BBD', // Blue
+  '#BD6CA4', // Magenta
+  '#4BB7BD', // Cyan
+  '#BFBFBF', // White
+  '#616161', // Bright Black
+  '#DC6C6C', // Bright Red
+  '#8CBC8C', // Bright Green
+  '#E6E600', // Bright Yellow
+  '#6B6BDB', // Bright Blue
+  '#BE89AE', // Bright Magenta
+  '#91D8DB', // Bright Cyan
+  '#E6E6E6'  // Bright White
 ]
 
 exports.decorateConfig = config => {
